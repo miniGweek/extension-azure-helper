6ccca041f9d0790f8392536dfb25eccb7b710eeb
Rahul Sarkar <rsarkar@sxiq.com.au>
2021-06-12 14:45:52 +1000

    Refactoring to support multiple short cuts/shortcut links - adding support for VM - IAM blade

--- azure-helper/azureHelper.js
+++ azure-helper/azureHelper.js
@@ -1,51 +1,104 @@
 main();
 
 function main() {
-  var trackIndex = "v6";
+  var trackIndex = "v7";
   console.log("AzureHelper extension " + trackIndex + " loaded");
-  var azurePortalSearchBox = "div.fxs-search-box.fxs-topbar-input>input";
-  var virtualMachineSearchAnchor =
-    "a.fxs-menu-item.fxs-search-menu-content[title*='Virtual machine']";
-  console.log("Loaded Azure Helper");
   //Attach to the search button
-  var searcher = null;
   setTimeout(function () {
-    $(azurePortalSearchBox).keydown(function (e) {
-      if (searcher == null) {
-        searcher = setInterval(function () {
+    $(extension_azure_helper.constants.azurePortalSearchBox).keydown(function (
+      e
+    ) {
+      if (extension_azure_helper.timers.searcher == null) {
+        extension_azure_helper.timers.searcher = setInterval(function () {
           console.log(
             "Searching search drop down again " +
               trackIndex +
               ", Interval timer is " +
-              searcher
+              extension_azure_helper.timers.searcher
           );
-          $(virtualMachineSearchAnchor).each(function (i, e) {
-            let resultRow = jQuery(e);
-            let vmAssetHref = resultRow.attr("href");
-            let splits = vmAssetHref.split("/");
-            let subscription = splits[5];
-            let resourcegroup = splits[7];
-            let vmname = splits[11];
-            let bastionLink = `https://portal.azure.com/#@globalasahi.com/resource/subscriptions/${subscription}/resourceGroups/${resourcegroup}/providers/Microsoft.Compute/virtualMachines/${vmname}/bastionHost`;
-
-            if (resultRow.parent().next("a:contains('Bastion')").length == 0) {
-              resultRow.parent().after(`
-          <a href="${bastionLink}" target='_blank'>Bastion</a>`);
-              console.log(
-                "added bastion link. clearing search interval " + searcher
-              );
-              resultRow
-                .parent()
-                .next("a:contains('Bastion')")
-                .click(function (e) {
-                  console.log("Bastion Link clicked");
-                  console.log("Clearing interval searching");
-                  clearInterval(searcher);
-                });
-            }
-          });
+          extension_azure_helper.vm_helper(
+            extension_azure_helper.constants.virtualMachineSearchAnchor
+          );
         }, 1000);
       }
     });
   }, 1000);
 }
+
+var extension_azure_helper = {
+  constants: {
+    baseAzureUrl:
+      "https://portal.azure.com/#@globalasahi.com/resource/subscriptions/",
+    azurePortalSearchBox: "div.fxs-search-box.fxs-topbar-input>input",
+    virtualMachineSearchAnchor:
+      "a.fxs-menu-item.fxs-search-menu-content[title*='Virtual machine']",
+    searchResultList: "fxs-portal-hover.fxs-menu-result-item",
+  },
+  timers: {
+    searcher: null,
+  },
+  vm_helper: function (virtualMachineSearchAnchor) {
+    $(virtualMachineSearchAnchor).each(function (i, e) {
+      let resultRow = jQuery(e);
+      let vmAssetHref = resultRow.attr("href");
+      let splits = vmAssetHref.split("/");
+      let subscription = splits[5];
+      let resourcegroup = splits[7];
+      let vmname = splits[11];
+      let bastionLink = `${extension_azure_helper.constants.baseAzureUrl}${subscription}/resourceGroups/${resourcegroup}/providers/Microsoft.Compute/virtualMachines/${vmname}/bastionHost`;
+      let iamLink = `${extension_azure_helper.constants.baseAzureUrl}${subscription}/resourceGroups/${resourcegroup}/providers/Microsoft.Compute/virtualMachines/${vmname}/users`;
+      extension_azure_helper.link_helper(
+        resultRow,
+        "Bastion",
+        bastionLink,
+        extension_azure_helper.timers.searcher
+      );
+      extension_azure_helper.link_helper(
+        resultRow,
+        "IAM",
+        iamLink,
+        extension_azure_helper.timers.searcher
+      );
+    });
+  },
+  link_helper: function (resultRow, shortCutName, shortCutLink, timer) {
+    let linkExists = extension_azure_helper.link_exists_checker(
+      resultRow,
+      shortCutName
+    );
+    if (!linkExists) {
+      resultRow.parent().after(`
+  <a class=${shortCutName} href="${shortCutLink}" target='_blank'>${shortCutName}</a>`);
+      console.log(
+        `added ${shortCutName} link. clearing search interval ${extension_azure_helper.timers.searcher}`
+      );
+      // resultRow
+      //   .parent()
+      //   .next(`a:contains(${shortCutName})`)
+      //   .click(function (e) {
+      //     console.log(`${shortCutName} Link clicked`);
+      //     console.log(`Clearing interval searching`);
+      //     clearInterval(timer);
+      //   });
+    }
+  },
+  link_exists_checker: function (resultRow, shortCutName) {
+    let next = resultRow.parent();
+    let stop = false;
+    let found = false;
+    while (!stop) {
+      if (next.next("a").hasClass(shortCutName)) {
+        stop = true;
+        found = true;
+      }
+      if (
+        next.next(`li.${extension_azure_helper.constants.searchResultList}`)
+          .length > 0
+      ) {
+        stop = true;
+      }
+      next = next.next();
+    }
+    return found;
+  },
+};
